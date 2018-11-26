3281a1a1673eeb6a1b0ac94bf5616a1752c8e1c9
H B <phajib@gmail.com>
2018-11-26 22:08:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,6 @@
 returns an array of string containing what instruments each musician plays*/
 function theBeatlesPlay(musicians, instruments){
   var array = []
-  //const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
-  //const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
   for(let i = 0; i < musicians.length; i++){
     array.push(`${musicians[i]} plays ${instruments[i]}`)
   }return array
