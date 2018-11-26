62064e41fdba712dcb08b1c79f478dc677933322
H B <phajib@gmail.com>
2018-11-26 22:08:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ function theBeatlesPlay(musicians, instruments){
 returns an array of string with exclamation points*/
 function johnLennonFacts(facts){
   var i = 0
-  //var array = [facts]
 
   while (i < facts.length){
       facts[i] = facts[i] + '!!!'
